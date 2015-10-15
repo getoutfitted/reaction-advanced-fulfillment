fc736fa4dd76a10a0601de89bd8ddafb6a356d6e
Paul Grever <paulgrever@gmail.com>
2015-10-15 10:52:20 -0600

    styled the orders page and made them clickable

--- client/templates/fulfillmentOrders/fulfillmentOrders.js
+++ client/templates/fulfillmentOrders/fulfillmentOrders.js
@@ -1,9 +1,23 @@
 Template.fulfillmentOrder.helpers({
   shippingDate: function () {
     let longDate = this.advancedFulfillment.shipmentDate;
-    return moment(longDate).format('MMMM Do YYYY');
+    return moment(longDate).format('MMMM Do, YYYY');
   },
   status: function () {
     return this.advancedFulfillment.workflow.status;
+  },
+  contactInfo: function () {
+    return this.email || 'Checked Out As Guest';
+  },
+  uniqueItemsCount: function () {
+    return this.items.length;
+  },
+  totalItemsCount: function () {
+    let total = _.reduce(this.items, function (sum, item) {
+      return sum + item.quantity;
+    }, 0);
+    return total;
   }
 });
+
+

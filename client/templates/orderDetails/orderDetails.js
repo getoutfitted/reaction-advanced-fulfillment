968a627787b8f1edac58cf642e47e4cef5110261
Paul Grever <paulgrever@gmail.com>
2015-10-16 10:33:47 -0400

    added shipping date and return date

--- client/templates/orderDetails/orderDetails.js
+++ client/templates/orderDetails/orderDetails.js
@@ -1,6 +1,14 @@
 Template.orderDetails.helpers({
   status: function () {
     return this.advancedFulfillment.workflow.status;
+  },
+  shippingDate: function () {
+    let date = this.advancedFulfillment.shipmentDate;
+    return moment(date).format('MMMM Do, YYYY');
+  },
+  returnDate: function () {
+    let date = this.advancedFulfillment.returnDate;
+    return moment(date).format('MMMM Do, YYYY');
   }
 });
 Template.itemDetails.helpers({

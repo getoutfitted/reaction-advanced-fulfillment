84e56fd575af8c8c43035351feb9ad4ec1cc9b8d
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-10-21 11:52:01 -0600

    initial expectations are passing

--- tests/jasmine/server/integration/methods.js
+++ tests/jasmine/server/integration/methods.js
@@ -1,7 +1,13 @@
 describe('getoutfitted:reaction-advanced-fulfillment methods', function () {
   describe('advancedFulfillment/updateOrderWorkflow', function () {
-    it('should return true', function () {
-      expect(true).toEqual(true);
+    beforeEach(function () {
+      return ReactionCore.Collections.Orders.remove({});
+    });
+    it('should update the orderWorkflow', function () {
+      let Order = Factory.create('orderForAF');
+      spyOn(ReactionCore.Collections.Orders, 'update');
+      expect(Order.advancedFulfillment).toBeDefined();
+      expect(Order.advancedFulfillment.workflow.status).toEqual('new');
     });
   });
 });

4db6a8aad19dbac31ffafd3c0877956633404717
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-10-21 12:41:23 -0600

    in progress

--- tests/jasmine/server/integration/methods.js
+++ tests/jasmine/server/integration/methods.js
@@ -19,13 +19,21 @@ describe('getoutfitted:reaction-advanced-fulfillment methods', function () {
       Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
       expect(ReactionCore.Collections.Orders.update).toHaveBeenCalled();
     });
-    it('should update create a history object on order', function () {
+    it('should update create a history object on order with correct event and userID', function () {
       let Order = Factory.create('orderForAF');
       let User = Factory.create('user');
       spyOn(ReactionCore.Collections.Orders, 'update').and.callThrough();
       expect(Order.history.length).toEqual(0);
       Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
-      expect(ReactionCore.Collections.Orders.findOne(Order._id).history.length).toEqual(1);
+      let thisOrder = ReactionCore.Collections.Orders.findOne(Order._id);
+      expect(thisOrder.history.length).toEqual(1);
+      expect(thisOrder.history[0].event).toEqual('orderPicking');
+      expect(thisOrder.history[0].userId).toEqual(User._id);
+    });
+    it('should update the advancedFulfillment workflow', function() {
+      let Order = Factory.create('orderForAF');
+      let User = Factory.create('user');
+      spyOn(ReactionCore.Collections.Orders, 'update').and.callThrough();
     });
   });
 });

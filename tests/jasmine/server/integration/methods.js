891e542d643e32b6a9574f51f0d16408124a8b77
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-10-21 14:36:17 -0600

    advancedFulfillment/updateOrderWorkflow fully tested with 9 tests passing

--- tests/jasmine/server/integration/methods.js
+++ tests/jasmine/server/integration/methods.js
@@ -30,10 +30,60 @@ describe('getoutfitted:reaction-advanced-fulfillment methods', function () {
       expect(thisOrder.history[0].event).toEqual('orderPicking');
       expect(thisOrder.history[0].userId).toEqual(User._id);
     });
-    it('should update the advancedFulfillment workflow', function() {
+    it('should update the advancedFulfillment workflow', function () {
       let Order = Factory.create('orderForAF');
       let User = Factory.create('user');
       spyOn(ReactionCore.Collections.Orders, 'update').and.callThrough();
+      Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
+      let thisOrder = ReactionCore.Collections.Orders.findOne(Order._id).advancedFulfillment;
+      expect(thisOrder.workflow.status).toEqual('orderPicking');
+      expect(thisOrder.workflow.workflow.length).toEqual(1);
+      expect(thisOrder.workflow.workflow).toContain('orderCreated');
+    });
+    it('should throw an error if not passed a valid orderid', function () {
+      let User = Factory.create('user');
+      let Order = Factory.create('orderForAF');
+      spyOn(ReactionCore.Collections.Orders, 'update');
+      expect(function () {
+        return Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
+      }).not.toThrow();
+      expect(function () {
+        return Meteor.call('advancedFulfillment/updateOrderWorkflow', 1234, User._id, 'orderCreated');
+      }).toThrow();
+    });
+    it('should throw an error if not passed a valid userid', function () {
+      let User = Factory.create('user');
+      let Order = Factory.create('orderForAF');
+      spyOn(ReactionCore.Collections.Orders, 'update');
+      expect(function () {
+        return Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
+      }).not.toThrow();
+      expect(function () {
+        return Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, true, 'orderCreated');
+      }).toThrow();
+    });
+    it('should throw an error if not passed a valid status', function () {
+      let User = Factory.create('user');
+      let Order = Factory.create('orderForAF');
+      spyOn(ReactionCore.Collections.Orders, 'update');
+      expect(function () {
+        return Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
+      }).not.toThrow();
+      expect(function () {
+        return Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, true);
+      }).toThrow();
+    });
+    it('should be able to be called multiple times and update the history and work flow', function () {
+      let User = Factory.create('user');
+      let Order = Factory.create('orderForAF');
+      spyOn(ReactionCore.Collections.Orders, 'update').and.callThrough();
+      Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, 'orderCreated');
+      let thisOrder = ReactionCore.Collections.Orders.findOne(Order._id);
+      Meteor.call('advancedFulfillment/updateOrderWorkflow', Order._id, User._id, thisOrder.advancedFulfillment.workflow.status);
+      thisOrder = ReactionCore.Collections.Orders.findOne(Order._id);
+      expect(thisOrder.history.length).toBe(2);
+      expect(thisOrder.advancedFulfillment.workflow.workflow.length).toBe(2);
+      expect(thisOrder.advancedFulfillment.workflow.status).toBe('orderPacking');
     });
   });
 });

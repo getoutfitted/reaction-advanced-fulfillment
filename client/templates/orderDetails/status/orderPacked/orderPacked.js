041b2e500feede64d4f427d233a9f2e81065891f
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-12-10 19:50:15 -0700

    displayes the shipping info for shipstation

--- client/templates/orderDetails/status/orderPacked/orderPacked.js
+++ client/templates/orderDetails/status/orderPacked/orderPacked.js
@@ -2,13 +2,12 @@ Template.orderPacked.helpers({
   shippingInfo: function () {
     let shippingInfo = this.shipping[0].address;
     let afShipping = {};
-    afShipping.fullName = shippingInfo.fullname;
+    afShipping.fullName = shippingInfo.fullName;
     afShipping.address1 = shippingInfo.address1;
     afShipping.address2 = shippingInfo.address2;
     afShipping.city = shippingInfo.city;
     afShipping.state = shippingInfo.region;
-    afShipping.zipcode = shippingInfo.zipcode;
-
+    afShipping.zipcode = shippingInfo.postal;
     return afShipping;
   }
 });

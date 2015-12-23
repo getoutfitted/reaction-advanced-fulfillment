4c1c71118862350c01f04ece21063c3af318e8b2
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-12-22 19:53:26 -0700

    added UI for ski packages

--- client/templates/orderDetails/nonPickableItems/nonPickableItems.js
+++ client/templates/orderDetails/nonPickableItems/nonPickableItems.js
@@ -1,8 +1,20 @@
 Template.nonPickableItems.helpers({
+  skiPackagesPurchased: function () {
+    return this.advancedFulfillment.skiPackagesPurchased;
+  }
+});
+
+Template.damageCoverage.helpers({
   qty: function (type) {
     return this.advancedFulfillment.damageCoverage[type].qty;
   },
   subtotal: function (type) {
     return this.advancedFulfillment.damageCoverage[type].subtotal;
+  }
+});
+
+Template.skiPackages.helpers({
+  skiPackages: function () {
+    return this.advancedFulfillment.skiPackages;
   }
 });

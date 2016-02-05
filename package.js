1f222b4f3d8d572ced932e625dcaeb846fc98067
Spencer Norman <spencern@gmail.com>
2016-02-05 12:12:16 -0700

    bump versions

--- package.js
+++ package.js
@@ -18,8 +18,8 @@ Package.onUse(function (api) {
   api.use('http');
   api.use('underscore');
   api.use('standard-minifiers');
-  api.use('reactioncommerce:core@0.9.5');
-  api.use('reactioncommerce:reaction-accounts@1.5.2');
+  api.use('reactioncommerce:core@0.11.0');
+  api.use('reactioncommerce:reaction-accounts@1.6.2');
   api.use('iron:router@1.0.12');
   api.use('momentjs:moment@2.10.6');
   api.use('momentjs:twix@0.7.2');
@@ -128,16 +128,15 @@ Package.onUse(function (api) {
 
 
 Package.onTest(function (api) {
-  api.use('sanjo:jasmine@0.20.2');
+  api.use('sanjo:jasmine@0.21.0');
   api.use('underscore');
   api.use('dburles:factory@0.3.10');
-  api.use('velocity:html-reporter@0.9.0');
-  api.use('velocity:console-reporter@0.1.3');
+  api.use('velocity:html-reporter@0.9.1');
+  api.use('velocity:console-reporter@0.1.4');
   api.use('velocity:helpers');
   api.use('reactioncommerce:reaction-factories');
 
-  api.use('reactioncommerce:core@0.9.5');
-  api.use('reactioncommerce:bootstrap-theme');
+  api.use('reactioncommerce:core@0.11.0');
   api.use('getoutfitted:reaction-advanced-fulfillment');
 
   api.addFiles([

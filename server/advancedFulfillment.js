a06c3a1dc90fdbd0f4989167c5fc7d9be16f618c
Paul Grever <paulgrever@gmail.com>
2016-04-15 18:18:28 -0600

    basic order goes... but still need line items

--- server/advancedFulfillment.js
+++ server/advancedFulfillment.js
@@ -11,6 +11,37 @@ AdvancedFulfillment.Shipstation.createOrder = function (orderId) {
   check(orderId, String);
   const order = ReactionCore.Collections.Orders.findOne(orderId);
   if (order) {
+    // let shipstation = {
+    //   orderNumber: 40,
+    //   orderDate: new Date(),
+    //   orderStatus: 'awaiting_shipment',
+    //   billTo: {
+    //     name: 'Paul Grever',
+    //     company: 'GetOutfitted',
+    //     street1: '460 S Marion pkwy',
+    //     street2: '1602C',
+    //     street3: null,
+    //     city: 'Denver',
+    //     state: 'CO',
+    //     postalCode: '80209',
+    //     phone: '6082392471',
+    //     residential: true,
+    //     addressVerified: 'Address validated successfully'
+    //   },
+    //   shipTo: {
+    //     name: 'Paul Grever',
+    //     company: 'GetOutfitted',
+    //     street1: '460 S Marion pkwy',
+    //     street2: '1602C',
+    //     street3: null,
+    //     city: 'Denver',
+    //     state: 'CO',
+    //     postalCode: '80209',
+    //     phone: '6082392471',
+    //     residential: true,
+    //     addressVerified: 'Address validated successfully'
+    //   }
+    // };
     let shipstation = {};
     shipstation.orderNumber = order.orderNumber;
     shipstation.orderDate = order.createdAt;
@@ -18,29 +49,65 @@ AdvancedFulfillment.Shipstation.createOrder = function (orderId) {
     // Billing
     shipstation.billTo = billTo = {};
     let billingInfo = order.billing[0].address;
-    billTo.name = billingInfo.fullname;
+    billTo.name = billingInfo.fullName;
     billTo.street1 = billingInfo.address1;
     billTo.street2 = billingInfo.address2;
+    billTo.street3 = null;
     billTo.city = billingInfo.city;
     billTo.state = billingInfo.region;
     billTo.postalCode = billingInfo.postal;
     billTo.phone = billingInfo.phone;
+    billTo.company = null;
     billTo.residential = billingInfo.isCommercial;
     billTo.addressVerified = 'Address not yet validated';
     // Shipping
     shipstation.shipTo = shipTo = {};
     let shippingInfo = order.shipping[0].address;
-    shipTo.name = shippingInfo.fullname;
+    shipTo.name = shippingInfo.fullName;
     shipTo.street1 = shippingInfo.address1;
     shipTo.street2 = shippingInfo.address2;
+    // shipTo.street3 = null;
     shipTo.city = shippingInfo.city;
     shipTo.state = shippingInfo.region;
     shipTo.postalCode = shippingInfo.postal;
     shipTo.phone = shippingInfo.phone;
     shipTo.residential = shippingInfo.isCommercial;
+    // shipTo.company = null;
     shipTo.addressVerified = 'Address not yet validated';
 
     // Optional But wanted for Our Use!
-    shipstation.shipByDate
+    // shipstation.shipByDate = order.advancedFulfillment.shipmentDate;
+    // shipstation.customerUsername = order.billing[0].address.fullName;
+    // shipstation.customerEmail = order.email || 'Email Not Provided';
+    // shipstation.amountPaid = order.billing[0].invoice.total;
+    // shipstation.shippingAmount = order.billing[0].invoice.shipping;
+    // shipstation.taxAmount = order.billing[0].invoice.taxes;
+    // shipstation.items = [];
+    // _.each(order.items, function (item) {
+    //   let i = {
+    //     orderItemId: item._id,
+    //     lineItemKey: item.productId,
+    //     quantity: item.quantity,
+    //     unitPrice: item.variants.price,
+    //     weight: {
+    //       value: item.variants.weight,
+    //       units: "pounds"
+    //     },
+    //     warehouseLocation: item.variants.location,
+    //     productId: item.variants._id,
+    //     options: [
+    //       {
+    //         name: 'size',
+    //         value: item.variants.size
+    //       },
+    //       {
+    //         name: 'color',
+    //         value: item.variants.color
+    //       }
+    //     ]
+    //   }
+    //   shipstation.items.push(i);
+    // });
+    Shipstation.createOrder(shipstation);
   }
 };

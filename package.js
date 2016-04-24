2455a61c03d3e443b01bc4a41f97aa6ce6eacdbd
Spencer Norman <spencern@gmail.com>
2016-04-24 01:53:57 -0600

    Bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Advanced fulfillment tracking for orders through inbound, picking, packing, returns and inventory',
   name: 'getoutfitted:reaction-advanced-fulfillment',
-  version: '0.7.1',
+  version: '0.8.0',
   git: 'https://github.com/getoutfitted/reaction-advanced-fulfillment'
 });
 

58414bf6848e8d4995c65860de96ce51bd825d04
Paul Grever <paulgrever@gmail.com>
2016-06-06 17:13:04 -0600

    added markdwn: true and made a few things bold

--- server/methods/slack.js
+++ server/methods/slack.js
@@ -8,19 +8,20 @@ Meteor.methods({
       let end = moment(order.endTime).format('M/D/YY');
       let orderNumber = `Order #${order.orderNumber} items: `;
       let orderLink = `${process.env.ROOT_URL}/dashboard/advanced-fulfillment/order/${order._id}`;
-      let orderText = `Order #${order.orderNumber} placed by: ${order.billing[0].address.fullName}
+      let orderText = `*Order #${order.orderNumber}* placed by: ${order.billing[0].address.fullName}
         Placed on: ${moment(order.createdAt).format('M/D/YY')}
         Rental Dates: ${start}-${end}
         Contact Email: ${order.email}
         Contact Phone: ${order.billing[0].address.phone}
         Shipping To: ${order.shipping[0].address.city}, ${order.shipping[0].address.region}
-        Order Total: $ ${order.billing[0].invoice.total}`;
+        *Order Total: $ ${order.billing[0].invoice.total}*`;
       let slackPost = {
         channel: 'sales',
         text: orderText,
         icon_url: 'https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Money-Increase-128.png',
         as_user: false,
-        username: 'sales_bot'
+        username: 'sales_bot',
+        mrkdwn: true
       };
 
       let attachments = {
